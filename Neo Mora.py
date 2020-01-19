498c620d4e78ebd79d0b3d7692fdef0510adbb62
Quinn Pham <quinnlp12@gmail.com>
2020-01-19 02:45:36 -0700

    dsaf

--- Neo Mora.py
+++ Neo Mora.py
@@ -1,14 +1,48 @@
 import random
 
-def CPUNumber():
-    return random.ranint(0, 5)
+def main():
+    while(1):
+        PVEGame()
 
-def CPUGuess(guess):
-    return random.randint(guess, guess + 5)
 
 class PVEGame():
 
-    def PVEplay():
-        
+    def PVEplay(self):
+
+        print("Hello, you are playing NEO MORA!")
+        print("PLAY YOUR HAND: ")
+        pnumber = self.getPlayerNumber()
+        print("YOUR OPPONENT HAS SELECTED A NUMBER")
+        cnumber = self.CPUGuess()
+        print("MAKE YOUR GUESS")
+        pguess = self.getPlayerNumber()
+        print("YOUR OPPONENT HAS MADE THEIR GUESS")
+        cguess = self.getPlayerGuess(cnumber)
+
+        total = pnumber + cnumber
+
+        print("YOU GUESSED %d" % pguess)
+        print("YOUR OPPONENT GUESSED %d" % cguess)
+        print("THE TOTAL WAS %d" % total)
+        if ((pguess == total) and(cguess == total)):
+            print("DRAW")
+        elif pguess == total:
+            print("YOU WIN")
+        elif cguess == total:
+            print("YOUR OPPONENT WON")
+            print("YOU LOSE")
+        else:
+            print("YOU BOTH LOSE")
+
+
+
+    def getPlayerNumber(self):
+        return input()
+    
+    def CPUNumber(self):
+        return random.ranint(0, 5)
+
+    def CPUGuess(self, guess):
+        return random.randint(guess, guess + 5)
 
 main()
