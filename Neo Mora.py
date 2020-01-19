c08ad47a56bf7f298c507f358cb9cbbc43f5458e
Quinn Pham <quinnlp12@gmail.com>
2020-01-19 03:02:45 -0700

    pve done

--- Neo Mora.py
+++ Neo Mora.py
@@ -1,46 +1,66 @@
 import random
 
 def main():
+    game = PVEGame()
     while(1):
-        PVEGame()
+        try: 
+            game.play()
+        except:
+            print("yo stop cheating")
 
 
 class PVEGame():
 
-    def PVEplay(self):
+    def play(self):
 
         print("Hello, you are playing NEO MORA!")
-        print("PLAY YOUR HAND: ")
+        print()
         pnumber = self.getPlayerNumber()
-        print("YOUR OPPONENT HAS SELECTED A NUMBER")
-        cnumber = self.CPUGuess()
-        print("MAKE YOUR GUESS")
-        pguess = self.getPlayerNumber()
-        print("YOUR OPPONENT HAS MADE THEIR GUESS")
-        cguess = self.getPlayerGuess(cnumber)
+        print("Your Opponent played a number")
+        cnumber = self.CPUNumber()
+    
+        pguess = self.getPlayerGuess()
+        print("Your Oppenent has guessed")
+        cguess = self.CPUGuess(cnumber)
 
         total = pnumber + cnumber
-
-        print("YOU GUESSED %d" % pguess)
-        print("YOUR OPPONENT GUESSED %d" % cguess)
-        print("THE TOTAL WAS %d" % total)
+        print()
+        print("Your Opponent played: %d" % cnumber)
+        print("You guessed: %d" % pguess)
+        print("Your Opponent guessed: %d" % cguess)
+        print("The total was: %d + %d = %d" % (pnumber, cnumber, total))
         if ((pguess == total) and(cguess == total)):
             print("DRAW")
         elif pguess == total:
-            print("YOU WIN")
+            print("WIN")
         elif cguess == total:
-            print("YOUR OPPONENT WON")
-            print("YOU LOSE")
+            print("LOSE")
         else:
             print("YOU BOTH LOSE")
+        print()
 
 
 
     def getPlayerNumber(self):
-        return input()
+        num  = input("Play a number: ")
+        if not num.isnumeric():
+            raise Exception
+        elif ((int(num) < 0) or (int(num) > 5)):
+            raise Exception
+        else:
+            return int(num)
     
+    def getPlayerGuess(self):
+        num  = input("Make your guess: ")
+        if not num.isnumeric():
+            raise Exception
+        elif ((int(num) < 0) or (int(num) > 10)):
+            raise Exception
+        else:
+            return int(num)
+
     def CPUNumber(self):
-        return random.ranint(0, 5)
+        return random.randint(0, 5)
 
     def CPUGuess(self, guess):
         return random.randint(guess, guess + 5)
