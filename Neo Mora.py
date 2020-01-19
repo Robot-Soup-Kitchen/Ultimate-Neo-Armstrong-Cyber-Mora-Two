5684c568f4bd343489f9fd1ecf7aec872c4d4c14
Quinn Pham <quinnlp12@gmail.com>
2020-01-19 02:31:14 -0700

    started lets ggooo

--- Neo Mora.py
+++ Neo Mora.py
@@ -1,1 +1,14 @@
+import random
+
+def CPUNumber():
+    return random.ranint(0, 5)
+
+def CPUGuess(guess):
+    return random.randint(guess, guess + 5)
+
+class PVEGame():
+
+    def PVEplay():
+        
+
 main()
